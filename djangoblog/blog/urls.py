a89576a878ece247899432807080ef931e7ae429
samuele-mattiuzzo <samumatt@gmail.com>
2013-08-19 20:40:00 +0100

    Urls

--- djangoblog/blog/urls.py
+++ djangoblog/blog/urls.py
@@ -5,8 +5,12 @@ urlpatterns = patterns('blog.views',
     # homepage view
     url(r'^$', 'homepage_view', name='homepage_view'),
 
+    # archive page view
+    url(r'archive/(?P<year>\d{4})/$', 
+        'archive_view', name='archive_view'),
+
     # single post view
-    url(r'(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[\w-]+)$', 
+    url(r'(?P<year>\d{4})/(?P<month>\d{1-2})/(?P<day>\d{1-2})/(?P<slug>[\w-]+)$', 
         'post_view', name='post_view'),
 
     # about me view
